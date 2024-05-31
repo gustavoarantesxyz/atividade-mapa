5793ea547ffb9ef5386426dc05564ae3d91450be
Gustavo Arantes <gustavoarantes@tuta.io>
2024-05-31 12:34:21 -0300

    Criado a classe Task no arquivo Task.java, contendo a logica para a tarefa.

---
+++ Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    private static int count = 0;
+    private int id;
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.id = ++count;
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + description + " [" + (isCompleted ? "Concluída" : "Pendente") + "]";
+    }
+}
